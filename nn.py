c33eb10bdf9142468146e2481e7e8c529279fb8c
tarasivashchuk <ivashchuktaras@gmail.com>
2019-10-19 02:11:37 -0400

    Added a DenseLayer class with forward propagation Added a generator for normal distribution data
    Signed-off-by: tarasivashchuk <ivashchuktaras@gmail.com>

--- nn.py
+++ nn.py
@@ -1,14 +1,34 @@
-"""
-This is a file to test initial code.
-"""
+import numpy as np
 
-data = [5.5, 10.6, 15.4]
-data_weight = 0.2
+from util import generate_data
 
 
-def neuron(nn_input, nn_weight):
-    return nn_input * nn_weight
+class Dense(object):
+    def __init__(self, input_shape, layer_size, activation):
+        self.weights = np.random.randn(layer_size, input_shape)
+        self.bias = np.zeros((layer_size, 1))
+        self.activation = activation
+
+    def forward(self, input_data):
+        # TODO: Switch around the shapes so that weights are transposed
+        input_data = np.reshape(input_data, (1, 20))
+        return self.activation(np.add(np.dot(self.weights, input_data.T), self.bias))
 
 
-for example in data:
-    print(neuron(example, data_weight))
+if __name__ == '__main__':
+    x, y = generate_data.normal_dataset(0., 10., 20)
+    tanh = lambda output: np.tanh(output)
+    linear = lambda output: output
+    # print(x.shape)
+    layer_1 = Dense(x.shape[0], 5, linear)
+    print(layer_1.forward(x))
+
+# data_weight = 0.2
+#
+#
+# def neuron(nn_input, nn_weight):
+#     return nn_input * nn_weight
+#
+#
+# for example in data:
+#     print(neuron(example, data_weight))
