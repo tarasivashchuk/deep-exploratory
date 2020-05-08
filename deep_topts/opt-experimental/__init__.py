37944eff180d428583faff1d1054bc0678f124ee
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-05-08 18:53:57 -0400

    Updated ignore file with IDE directories
    - Also untracked and removed Pycharm settings folder
    
    Signed-off-by: Taras Ivashchuk <taras@tarasivashchuk.com>

--- deep_topts/opt-experimental/__init__.py
+++
@@ -1,7 +0,0 @@
-#  Copyright (c)  2019. Taras Ivashchuk
-
-"""
-This package is for testing out experimental ideas for machine learning such as trying out new types of neural networks 
-and machine learning algortithms, training algorithms, loss functions, and anything else I happen to think of. Pytorch
-is the standard library used for experiments currently.
-"""
